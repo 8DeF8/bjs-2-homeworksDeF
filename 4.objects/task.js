656d3ab8e6b795c6db970f2498b48fd0a48e6702
DeF <DeF-2008@yandex.ru>
2024-07-09 11:07:48 +0300

    hw

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,31 +3,30 @@ function Student(name, gender, age) {
     this.gender = gender;
     this.age = age;
     this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
-}
-
-Student.prototype.addMarks = function (...marks) {
+  }
+  
+  Student.prototype.addMarks = function (...marks) {
+   
     if (!this.hasOwnProperty('marks')) {
-        console.log("Студент отчислен.");
-        return;
+      console.log("Студент отчислен!");
+      return;
     }
     this.marks.push(...marks);
-}
-
-Student.prototype.getAverage = function () {
+  }
+  
+  Student.prototype.getAverage = function () {
     if (!this.marks || this.marks.length === 0) {
-        return 0;
+      return 0;
     }
     return this.marks.reduce((acc, currentValue) => acc + currentValue) / this.marks.length;
-}
-
-Student.prototype.exclude = function (reason) {
+  }
+  
+  Student.prototype.exclude = function (reason) {
     delete this.subject;
     delete this.marks;
-    this.exclude = reason;
-  
-}
+    this.excluded = reason;
+  }
